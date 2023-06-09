045897eb77dc0d9c9ec85c43bb8b766ca5156d1f
Robert Jensen <robert@robert-jensen.dk>
2023-06-09 21:03:06 +0200

    updated address

--- src/components/pages/home/venue/venue.jsx
+++ src/components/pages/home/venue/venue.jsx
@@ -16,7 +16,7 @@ const Venue = () => (
           cloud native communities, supported by the Cloud Native Computing Foundation (CNCF).
         </p>
         <p className="mt-7">
-          This is the first KCD in <span className="font-bold">Switzerland</span>, and we’re excited
+          This is the first KCD in <span className="font-bold">Denmark</span>, and we’re excited
           to bring the community together. We want to provide a platform for professionals and
           experts from all levels and backgrounds to learn, network, and share their knowledge on
           all things cloud native.
@@ -39,24 +39,21 @@ const Venue = () => (
             />
             <div className="pt-5 pb-8 pl-10 pr-16 leading-normal md:row-start-1 md:px-6 sm:row-start-2">
               <Link
-                to="https://goo.gl/maps/ooQufQWKzaRM286Z7"
+                to="https://goo.gl/maps/aegmwLjH3CxsY8wm7"
                 className="text-2xl font-semibold sm:text-xl"
                 theme="primary"
               >
-                Google HQ <br />
-                at Zurich Europaallee
+                Jyst HQ
               </Link>
               <address className="mt-5 text-lg not-italic text-[#262F59] opacity-60 sm:text-lg">
-                Google EURF <br /> Europaallee 36 <br /> 8004 Zürich
+                Jysk <br /> Sødalsparken 18 <br /> 8220, Brabrand
               </address>
             </div>
           </div>
 
           <div className="w-[520px] self-center text-lg leading-normal text-primary-1 xl:max-w-[470px] lg:ml-8 lg:max-w-[530px] md:ml-0 md:mt-5 md:w-full md:max-w-none sm:text-lg">
             <p>
-              The Kubernetes Community Days in Zurich will be held at the Google HQ at Zurich
-              Europaallee, a modern and convenient location next to the train station. Attendees can
-              expect to enjoy a variety of vegetarian and vegan food options throughout the day.
+              The Kubernetes Community Days in Copenhagen will be held at the Jysk HQ in Brabrand.
             </p>
             <p className="mt-5">
               Don’t miss out on the opportunity to connect with fellow attendees and continue the
