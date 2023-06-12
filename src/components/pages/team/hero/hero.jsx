3fcd6da817b93515f1ca44e40cbfaacfdec20322
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2023-06-12 20:40:30 +0200

    Prepare Team page with placeholders
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- src/components/pages/team/hero/hero.jsx
+++ src/components/pages/team/hero/hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TITLE = 'Team';
 
 const DESCRIPTION =
-  'The Kubernetes Community Days in Zurich are organized by a dedicated committee who are passionate about bringing people together and fostering a sense of community. Our goal is to provide a platform for like-minded individuals from all levels and backgrounds that is dedicated to learning, growth, and diversity.';
+  'The Kubernetes Community Days in Denmark are organized by a dedicated committee who are passionate about bringing people together and fostering a sense of community. Our goal is to provide a platform for like-minded individuals from all levels and backgrounds that is dedicated to learning, growth, and diversity.';
 
 const Hero = () => (
   <section className="safe-paddings safe-paddings overflow-hidden bg-[#EDC3C7] bg-opacity-10 pt-24 pb-20">
