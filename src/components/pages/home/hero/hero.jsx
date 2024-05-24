94f99c4a1514056bb5ffa29089c0e16566f3a37f
Camilla <36456454+tuxiem@users.noreply.github.com>
2024-05-24 10:15:17 +0200

    changed button to early bird tickets (#135)

--- src/components/pages/home/hero/hero.jsx
+++ src/components/pages/home/hero/hero.jsx
@@ -42,8 +42,7 @@ const Hero = () => (
               to="https://kcddenmark-3.ticketbutler.io/en/e/kcd-denmark-2024-1/"
               size="lg"
               theme="secondary"
-            >Get tickets</Button>
-
+            >Early Bird Tickets</Button>
             {/* <Button
               className="mt-5 rounded-3xl bg-red px-20 py-2 text-xl font-bold leading-none text-white shadow-[0_15px_40px_#999999] hover:shadow-[0px_15px_30px_#adadad]"
               to="/venue"
