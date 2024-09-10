d6bbba76e2ed3b7c6e7e858a847f13ef4f0284d8
Thor Anker Kvisgård Lange <tal@netic.dk>
2024-09-10 08:18:44 +0200

    feat: :tada: Adding CFP schedule (#163)

--- src/components/pages/home/proposal/proposal.jsx
+++ src/components/pages/home/proposal/proposal.jsx
@@ -4,8 +4,6 @@ import Button from 'components/shared/button';
 import LINKS from 'constants/links';
 
 const TITLE = 'Call for Proposal';
-const DESCRIPTION = "Call for Proposals are now closed. We are currently in review of those proposals which were submitted. Watch this space for the schedule release, or follow us on LinkedIn/X."
-  //"If you are an expert or newcomer in the cloud native computing world and would like to share your knowledge and experience with the community, we encourage you to submit a proposal. Our committee is looking for diverse topics and speakers, so don't hesitate to submit your ideas. We can't wait to see what the community offers, and we look forward to receiving your submissions.";
 
 const Proposal = () => (
   <section className="safe-paddings relative bg-[#EDC3C7] bg-opacity-10 py-20">
@@ -15,42 +13,48 @@ const Proposal = () => (
         <h2 className="text-6xl font-bold leading-tight" id={LINKS.proposal.id}>
           {TITLE}
         </h2>
-        <p className="mt-8 text-2xl leading-normal sm:text-lg">{DESCRIPTION}</p>
+        <p className="mt-8 text-2xl leading-normal sm:text-lg">
+          Call for Proposals are now closed. We are currently in review of those proposals which
+          were submitted. Watch this space for the schedule release, or follow us on{' '}
+          <a href="https://www.linkedin.com/company/kubernetes-community-days-denmark/">LinkedIn</a>.
+        </p>
 
-        <div className="container py-10 mx-auto overflow-x-auto">
-  <table className="table-auto text-base border-collapse border mx-auto">
-    <thead className="border-b font-medium dark:border-neutral-500">
-      <tr>
-        <th className="px-4 py-4">Activity</th>
-        <th className="px-4 py-4">Date</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr className="border-b dark:border-neutral-500">
-        <td className="px-4 py-4 font-medium">CFP Opens</td>
-        <td className="px-4 py-4">1st August, 2023</td>
-      </tr>
-      <tr className="border-b dark:border-neutral-500">
-        <td className="px-4 py-4 font-medium"><strong>CFP Closes</strong></td>
-        <td className="px-4 py-4"><strong>15th September, 2023</strong></td>
-      </tr>
-      <tr className="border-b dark:border-neutral-500">
-        <td className="px-4 py-4 font-medium">CFP Notifications</td>
-        <td className="px-4 py-4">1st October, 2023</td>
-      </tr>
-      <tr className="border-b dark:border-neutral-500">
-        <td className="px-4 py-4 font-medium">Schedule Announced</td>
-        <td className="px-4 py-4">7th October, 2023</td>
-      </tr>
-      <tr className="border-b dark:border-neutral-500">
-        <td className="px-4 py-4 font-medium">Slides Submission</td>
-        <td className="px-4 py-4">7th November, 2023</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-
-
+        <div className="container mx-auto overflow-x-auto py-10">
+          <table className="mx-auto table-auto border-collapse border text-base">
+            <thead className="dark:border-neutral-500 border-b font-medium">
+              <tr>
+                <th className="px-4 py-4">Activity</th>
+                <th className="px-4 py-4">Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="dark:border-neutral-500 border-b">
+                <td className="px-4 py-4 font-medium">CFP Opens</td>
+                <td className="px-4 py-4">1st July, 2024</td>
+              </tr>
+              <tr className="dark:border-neutral-500 border-b">
+                <td className="px-4 py-4 font-medium">
+                  <strong>CFP Closes</strong>
+                </td>
+                <td className="px-4 py-4">
+                  <strong>1st September, 2024</strong>
+                </td>
+              </tr>
+              <tr className="dark:border-neutral-500 border-b">
+                <td className="px-4 py-4 font-medium">CFP Notifications</td>
+                <td className="px-4 py-4">20th September, 2024</td>
+              </tr>
+              <tr className="dark:border-neutral-500 border-b">
+                <td className="px-4 py-4 font-medium">Schedule Announced</td>
+                <td className="px-4 py-4">1st October, 2024</td>
+              </tr>
+              <tr className="dark:border-neutral-500 border-b">
+                <td className="px-4 py-4 font-medium">Slides Submission</td>
+                <td className="px-4 py-4">12th November, 2024</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </section>
